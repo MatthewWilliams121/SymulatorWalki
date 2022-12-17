d2b9e619bf2c6c8220bd6ee217a402591af3c1b7
MatthewWilliams121 <120844684+MatthewWilliams121@users.noreply.github.com>
2022-12-18 00:18:15 +0100

    Dodaj pliki projektów.

---
+++ index/Battlefield.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace index
+{
+    internal class Battlefield
+    {
+        static public Unit[] attacker = new Unit[3];
+        static public Unit[] defender = new Unit[3];
+        static private float get_attacker_value() { return 0; }
+        static private float get_defender_value() { return 0; }
+        static public void resolve() { }
+    }
+}
